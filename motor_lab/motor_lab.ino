dab1ee90a0e8c07a096f1a4d27af42326a3a1526
Sheldon Smith <shsmith5595@gmail.com>
2018-02-14 13:27:29 -0500

    Finish up pid and sensor/motor control. Stub out GUI control over Serial

--- motor_lab/motor_lab.ino
+++ motor_lab/motor_lab.ino
@@ -1,5 +1,4 @@
-
-
+#include <Servo.h>
 #include <A4988.h>
 #include <BasicStepperDriver.h>
 #include <DRV8825.h>
@@ -10,20 +9,25 @@
 
 const int encoderPinA = 2;
 const int encoderPinB = 6;
-volatile unsigned int encoderPos = 0;
-unsigned int lastReportedPos = 1;
-
-boolean A_set = false;
-boolean B_set = false;
+volatile int encoderPos = 0;
 
 const int potPin = A0;
 const int flexPin = A1;
 const int trigPin = 8;
 const int echoPin = 7;
 const int switchPin = A2;
+const int stepPin = 3; 
+const int dirPin = 4; 
+const int motor_steps = 200;
+A4988 stepper(motor_steps, dirPin, stepPin);
+const int en1 = 11;
+const int dir11 = 12;
+const int dir12 = 13;
+Servo myservo;
+int pos = 0;
 
 
-// Interrupt on A changing state
+// Encoder interrupt
 void doEncoder() {
   /* If pinA and pinB are both high or both low, it is spinning
      forward. If they're different, it's going backward.
@@ -36,73 +40,59 @@ void doEncoder() {
   } else {
     encoderPos--;
   }
-
-  //Serial.println (encoderPos, DEC);
 }
 
-//declare pins
-#include <Servo.h>
-//////////////
-// Servo Motor
-//////////////
-Servo myservo;
-int pos = 0; 
+void servo_init() {
+ myservo.attach(5);
+}
 
-///////////////////
-// Stepper Motor
-///////////////////
-const int stepPin = 3; 
-const int dirPin = 4; 
-const int motor_steps = 800;
-A4988 stepper(motor_steps, dirPin, stepPin);
-
-///////////////////
-// DC Motor
-///////////////////
-const int en1 = 11;
-const int dir11 = 12;
-const int dir12 = 13;
- 
-//setup all pins as outputs
-void setup(){
-
+void encoder_init() {
+  
   pinMode(encoderPinA, INPUT);
   digitalWrite(encoderPinA, HIGH); 
   pinMode(encoderPinB, INPUT); 
   digitalWrite(encoderPinB, HIGH);
-
-// encoder pin on interrupt 0 (pin 2)
+  
+  // encoder pin on interrupt 0 (pin 2)
   attachInterrupt(0, doEncoder, CHANGE);
+}
 
+void switch_init() {
   pinMode(switchPin, INPUT);
-  
-  Serial.begin(9600);
-  ////////////////////
-  // Servo Motor
-  ///////////////////
-  myservo.attach(5);
-  
-  ////////////////////
-  // Stepper Motor
-  ////////////////////
-  stepper.begin(1,1);
+}
 
-  ///////////////////
-  // DC Motor
-  ///////////////////
+void stepper_init() {
+  stepper.begin(10,1);
+}
+
+void motor_init() {
   pinMode(en1, OUTPUT);
   pinMode(dir11, OUTPUT);
   pinMode(dir12, OUTPUT);
+}
 
+void ultrasound_init() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+}
+
+void setup(){
+
+  servo_init();
+  encoder_init();
+  switch_init();
+  stepper_init();
+  motor_init();
+  ultrasound_init();
+
+  Serial.begin(9600);
+
 }
 
 void writeMotor(char motor, int val)
 {
   if (motor == 'r'){
-    myservo.write(val);
-    delay(1000);
+    move_servo(val);
     return;
   }
   if (motor == 'd'){
@@ -110,72 +100,103 @@ void writeMotor(char motor, int val)
     return;
   }
   if (motor == 's'){
-    stepper.rotate(val);
-    delay(5000);
+    move_stepper(val);
+    return;
+  }
+  if (motor == 'r') {
+    motor_PID(val);
     return;
   }
     return;
- }
-
-void loop(){
-  delay(5000);
-  if (Serial.available()) {
-    char motorSpecifier = Serial.read();
-    int motorPos = Serial.parseInt();
-    writeMotor(motorSpecifier, motorPos);
-    }
-   delay(5000);
-
-   
-  /////////////////////
-  // Servo Motor
-  /////////////////////
-  
-//  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-//    // in steps of 1 degree
-//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-//    delay(15);                       // waits 15ms for the servo to reach the position
-//  }
-//  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-//    delay(15);                       // waits 15ms for the servo to reach the position
-//  }
-//  
-////  ////////////////////
-////  // Stepper Motor
-////  ////////////////////
-//
-//    stepper.rotate(10);
-    flex();
-    pot();
-    ultrasound();
 }
 
-void flex() {
+void move_servo(int pos) {
+  myservo.write(pos);
+  delay(15); //Delay to allow time for movement
+}
+
+void move_stepper(int pos) {
+  stepper.rotate(pos);
+}
+
+int previousFlexValue = NULL;
+
+void flex_stepper_control() {
+  int flexValue = flex();
+  if (previousFlexValue = NULL) {
+    previousFlexValue = flexValue;
+    move_stepper(flexValue);
+  }
+
+  else if(abs(previousFlexValue - flexValue) > 20) {
+    move_stepper(flexValue);
+  }
+
+  previousFlexValue = flexValue;
+}
+
+int flex() {
   
   int flexValue;
 
   flexValue = analogRead(flexPin);
   flexValue = .75*flexValue-620-25;
-// Printing cause problem to GUI control!
-//  Serial.print("Flex: ");
-//  Serial.println(flexValue);
-  stepper.rotate(flexValue);
+
+  return flexValue;
 }
-void pot() {
+
+void pot_servo_control() {
+  int potValue = pot();
+  move_servo(potValue*2/3);
+}
+
+int pot() {
 
     int potValue;
     
     potValue = analogRead(potPin);
     potValue = 0.2612*potValue;
-//    Serial.print("Pot: ");
-//    Serial.println(potValue);
-    myservo.write(potValue/4);
-    delay(1000);
+    
+    return potValue;
 }
 
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50; 
+int lastButtonState = LOW;
+int buttonState;
 
-void ultrasound() {
+int read_switch() {
+
+  int reading = digitalRead(switchPin);
+
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState)
+      buttonState = reading;
+  }
+  lastButtonState = reading;
+
+  return buttonState;
+  
+}
+
+void ultrasound_motor_control() {
+  int distance = ultrasound();
+  int motor_speed = distance*10;
+
+  if (motor_speed > 255) {
+    motor_speed = 255;
+  }
+
+  analogWrite(en1, motor_speed);
+  digitalWrite(dir11, HIGH);
+  digitalWrite(dir12, LOW);
+}
+
+int ultrasound() {
 
   long duration;
   int distance;
@@ -192,45 +213,86 @@ void ultrasound() {
 
   distance = duration*0.034/2;
 
-  ///////////////////
-  // DC Motor
-  ///////////////////
-  
-  if (digitalRead(switchPin)) {
-    motor_speed = distance*10;
-  } else {
-    motor_speed = 0;
-  }
-  
-  
-  if (motor_speed > 255) {
-    motor_speed = 255;
-  }
-  
-  analogWrite(en1, motor_speed);
-  digitalWrite(dir11, HIGH);
-  digitalWrite(dir12, LOW);
-
-//  Serial.print("Distance: ");
-//  Serial.println(distance);
+  return distance;
 }
 
-void setDCMotorSpeed(int motorSpeed) {
-  int motor_speed = 0;
-  if (digitalRead(switchPin)) {
-    motor_speed = motorSpeed;
-  } else {
-    motor_speed = 0;
-  }
-  
-  
-  if (motor_speed > 255) {
-    motor_speed = 255;
-  }
+void setDCMotorSpeed(int motor_speed) {
 
-  analogWrite(en1, motor_speed);
-  digitalWrite(dir11, HIGH);
-  digitalWrite(dir12, LOW);
-  delay(5000);
+  motor_speed *= 2.5;
+  
+  if (motor_speed >= 0) {
+    analogWrite(en1, motor_speed);
+    digitalWrite(dir11, HIGH);
+    digitalWrite(dir12, LOW);
+  }
+  if (motor_speed < 0) {
+    analogWrite(en1, motor_speed);
+    digitalWrite(dir11, LOW);
+    digitalWrite(dir12, HIGH);
+  }
 }
 
+void turn_motor(int distance, int output) {
+  int motor_speed = output*2.5;
+  if(distance > 0) {
+    analogWrite(en1, motor_speed);
+    digitalWrite(dir11, HIGH);
+    digitalWrite(dir12, LOW);
+  }
+  else if(distance < 0) {
+    analogWrite(en1, motor_speed);
+    digitalWrite(dir11, LOW);
+    digitalWrite(dir12, HIGH);
+  }
+  else {
+    analogWrite(en1, 0);
+    digitalWrite(dir11, LOW);
+    digitalWrite(dir12, HIGH);
+  }
+  
+}
+int oldDistance = 0;
+
+void motor_PID(int targetPos) {
+
+  int distance = targetPos - encoderPos;
+
+  int kterm = distance * 2; //kp
+  int iterm = 2 * (distance - oldDistance); //ki
+
+  int output = abs(kterm + iterm);
+
+  if (output > 100) {
+    output = 100;
+  }
+
+  if (output <= 10) {
+    output = 0;
+  }
+
+  oldDistance = distance;
+
+  turn_motor(distance, output);
+
+}
+
+int motorPos = 0;
+
+void loop(){
+
+  if(read_switch()) {
+    if (Serial.available()) {
+      char motorSpecifier = Serial.read();
+      int motorPos = Serial.parseInt();
+      writeMotor(motorSpecifier, motorPos);
+      }
+  } else {
+    flex_stepper_control();
+    pot_servo_control();
+    ultrasound_motor_control();
+  }
+  Serial.print(pot()); Serial.print(", ");
+  Serial.print(ultrasound()); Serial.print(", ");
+  Serial.println(flex());
+  delay(500);
+}
